d8f979dcccedab0c32ed2512b4a31b5484dd4d24
Pablo Scheinfeld <scheinpablo@yahoo.com>
2019-06-09 22:53:15 -0700

    clase label y editline

--- pruebaspablo.py
+++ pruebaspablo.py
@@ -1,14 +1,97 @@
+import sys
+
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtGui import QDoubleValidator
+from PyQt5.QtWidgets import QLineEdit, QLabel
 
-app = QtWidgets.QApplication([])
-dig = uic.loadUi("filter.ui")
-
-validator = QDoubleValidator(0, 100, 2)
-dig.editProperty1.setValidator(validator)
-
-dig.editProperty1.hide()
+from filter_ui import Ui_Filter
+from frontend_ui import Ui_MainWindow
+from input_ui import Ui_Input
 
 
-dig.show()
-app.exec()
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
+        self.setupUi(self)
+        # setear todos los callbacks de botones
+        self.entrance = EntranceDialog()
+        self.inputSelectButton.clicked.connect(self.clickedInput)
+        self.filterSelectButton.clicked.connect(self.clickedFilter)
+        self.outputConfigButton.clicked.connect(self.clickedOutput)
+
+    def clickedInput(self):
+        self.entrance.show()
+
+    def clickedFilter(self):
+        self.filter
+
+
+class EntranceDialog(QtWidgets.QDialog, Ui_Input):
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QDialog.__init__(self, *args, **kwargs)
+        self.setupUi(self)
+
+
+class FilterDialog(QtWidgets.QDialog, Ui_Filter):
+    pass
+
+
+class FilterParameterViews(QLineEdit, QLabel):
+    def __init__(self, line_edit, label):
+        self.propertyTitle = label
+        self.propertyLineEdit = line_edit
+        self.userInputValue = None
+        self.onlyNumbers = True
+        self.set_only_numbers(self.onlyNumbers)
+
+    def hide_all(self):
+        self.propertyLineEdit.hide()
+        self.propertyTitle.hide()
+
+    def show_all(self):
+        self.propertyLineEdit.show()
+        self.propertyTitle.show()
+
+    def update_user_input_value(self):
+        self.userInputValue = self.propertyLineEdit.text()
+
+    def get_user_input_value(self):
+        return self.userInputValue
+
+    def set_only_numbers(self, accept_only_numbers):
+        self.onlyNumbers = accept_only_numbers
+        if accept_only_numbers:
+            validator = QDoubleValidator()
+            self.propertyLineEdit.setValidator(validator)
+        else:
+            self.propertyLineEdit.setValidator(None)
+
+    def get_only_numbers(self):
+        return self.onlyNumbers
+
+    def set_property_title(self, title):
+        self.propertyTitle = title
+
+    def get_line_edit(self):
+        return self.propertyLineEdit
+
+    def get_label(self):
+        return self.propertyTitle
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    dig = uic.loadUi("filter.ui")
+
+    parameters1 = FilterParameterViews(dig.editProperty1, dig.titleProperty1)
+    parameters1.set_property_title("Primer cero")
+
+    parameters2 = FilterParameterViews(dig.editProperty2, dig.titleProperty2)
+    parameters2.set_property_title("Segundo cero")
+
+    parameters3 = FilterParameterViews(dig.editProperty3, dig.titleProperty3)
+    parameters3.hide_all()
+
+    dig.show()
+    app.exec()
