10d9ea9b10f8a13145a8713ea9d379e1443d89cd
Pablo Scheinfeld <scheinpablo@yahoo.com>
2019-06-16 22:53:53 -0700

    graph en otro metodo

--- pruebaspablo.py
+++ pruebaspablo.py
@@ -135,34 +135,60 @@ class DataEntryCombo:
         self.propertyTitle.setText(title)
 
 
+class Out:
+    @staticmethod
+    def return_out():
+        a = [10, 20, 30, 40, 75, 95, 120]
+        b = [60, -70, 80, 90, 65, 88, 77]
+        c = [10, 50, 80, 99, 120, 180, 222, 4000, 84444, 95555, 3333333, 5555555555555]
+        d = [20, 45, -88, 100, -151, 174, 188, 555, 800, 1050, 9999, 400]
+        e = [20, 20, 85, 85, -40, -280, 252]
+        f = [60, -60, 80, -80, 64, 55, 22]
+        graphic1 = GraphicProperties("Ceros", "Re", "Im", a, b, GraphicTypes.Zeros)
+        graphic2 = GraphicProperties("Polos", "Re", "Im", e, f, GraphicTypes.Poles)
+
+        graphics = [graphic1, graphic2]
+        return graphics
+
+
 class OutputGraphics(QMainWindow):
 
-    def __init__(self, graphics_properties):
+    def __init__(self):
         QMainWindow.__init__(self)
         loadUi("probandofront.ui", self)
         self.setWindowTitle("Salida")
-        self.graphics = graphics_properties
+        self.graphics = None
         self.nextGraphicToShow = None
         self.graphCounter = 0
         self.maxIndex = None
-
-        if isinstance(graphics_properties, GraphicProperties):
-            self.nextGraphicToShow = graphics_properties
-            self.prevGraphicButton.hide()
-            self.nextGraphicButton.hide()
-
-        elif isinstance(graphics_properties, list):
-            self.maxIndex = len(graphics_properties) - 1
-            self.prevGraphicButton.show()
-            self.nextGraphicButton.show()
-            self.nextGraphicToShow = graphics_properties[0]
-            self.prevGraphicButton.clicked.connect(self.previous_graph)
-            self.nextGraphicButton.clicked.connect(self.next_graph)
-            self.check_counter()
-
-        self.graphicateButton.clicked.connect(self.update_graph)
-        self.update_graph()
+        self.prevGraphicButton.clicked.connect(self.previous_graph)
+        self.nextGraphicButton.clicked.connect(self.next_graph)
+        self.fetch_graphics_information()
+        self.graphicateButton.clicked.connect(self.graph_button_action)
         self.addToolBar(NavigationToolbar(self.GraphWidget.canvas, self))
+
+    def graph_button_action(self):
+        self.fetch_graphics_information()
+        self.update_graph()
+
+    def fetch_graphics_information(self):
+        if Out.return_out() is not None:
+            self.graphics = None
+            self.nextGraphicToShow = None
+            self.graphCounter = 0
+            self.maxIndex = None
+            self.graphics = Out.return_out()
+            if isinstance(self.graphics, GraphicProperties):
+                self.nextGraphicToShow = self.graphics
+                self.prevGraphicButton.hide()
+                self.nextGraphicButton.hide()
+
+            elif isinstance(self.graphics, list):
+                self.maxIndex = len(self.graphics) - 1
+                self.prevGraphicButton.show()
+                self.nextGraphicButton.show()
+                self.nextGraphicToShow = self.graphics[0]
+                self.check_counter()
 
     def previous_graph(self):
         self.graphCounter -= 1
@@ -188,16 +214,18 @@ class OutputGraphics(QMainWindow):
             self.nextGraphicButton.show()
 
     def update_graph(self):
+
         self.GraphWidget.canvas.axes.clear()
         self.fix_axes_titles_position()
-        if self.nextGraphicToShow.graphicType == GraphicTypes.LINEAL:
+        if self.nextGraphicToShow.graphicType == GraphicTypes.Output:
 
             self.GraphWidget.canvas.axes.plot(self.nextGraphicToShow.xValueArray, self.nextGraphicToShow.yValueArray)
             self.GraphWidget.canvas.axes.set_title(self.nextGraphicToShow.title)
             self.GraphWidget.canvas.axes.grid()
             self.GraphWidget.canvas.draw()
 
-        elif self.nextGraphicToShow.graphicType == GraphicTypes.BODE:
+        elif self.nextGraphicToShow.graphicType == GraphicTypes.BodeModule or \
+                self.nextGraphicToShow.graphicType == GraphicTypes.BodePhase:
 
             self.GraphWidget.canvas.axes.semilogx(self.nextGraphicToShow.xValueArray,
                                                   self.nextGraphicToShow.yValueArray)
@@ -205,17 +233,28 @@ class OutputGraphics(QMainWindow):
             self.GraphWidget.canvas.axes.grid()
             self.GraphWidget.canvas.draw()
 
-        elif self.nextGraphicToShow.graphicType == GraphicTypes.zero_POLES:
+        elif self.nextGraphicToShow.graphicType == GraphicTypes.Zeros:
 
             self.GraphWidget.canvas.axes.spines['top'].set_color('none')
             self.GraphWidget.canvas.axes.spines['bottom'].set_position('zero')
             self.GraphWidget.canvas.axes.spines['left'].set_position('zero')
             self.GraphWidget.canvas.axes.spines['right'].set_color('none')
-            if self.nextGraphicToShow.zerosXValues is not None:
-                for i in range(len(self.nextGraphicToShow.zerosXValues)):
-                    self.GraphWidget.canvas.axes.plot(self.nextGraphicToShow.zerosXValues[i],
-                                                      self.nextGraphicToShow.zerosYValues[i], color='blue',
-                                                      markersize=10, marker='o')
+
+            for i in range(len(self.nextGraphicToShow.xValueArray)):
+                self.GraphWidget.canvas.axes.plot(self.nextGraphicToShow.xValueArray[i],
+                                                  self.nextGraphicToShow.yValueArray[i], color='blue',
+                                                  markersize=10, marker='o')
+
+            self.GraphWidget.canvas.axes.set_title(self.nextGraphicToShow.title)
+            self.GraphWidget.canvas.axes.grid()
+            self.GraphWidget.canvas.draw()
+
+        elif self.nextGraphicToShow.graphicType == GraphicTypes.Poles:
+
+            self.GraphWidget.canvas.axes.spines['top'].set_color('none')
+            self.GraphWidget.canvas.axes.spines['bottom'].set_position('zero')
+            self.GraphWidget.canvas.axes.spines['left'].set_position('zero')
+            self.GraphWidget.canvas.axes.spines['right'].set_color('none')
 
             for j in range(len(self.nextGraphicToShow.xValueArray)):
                 self.GraphWidget.canvas.axes.plot(self.nextGraphicToShow.xValueArray[j],
@@ -245,41 +284,27 @@ class OutputGraphics(QMainWindow):
 
 ##Class GraphicProperties
 # This class is used to unify the properties of the graphs to show.
-# In case you want to display a graph of POLES and ZEROS:
-#                   Use the parameter x_value_array for the x values of the poles.
-#                   Use the parameter y_value_array for the y values of the poles.
-#                   Use the parameter zeros_x_values for the x values of the zeros.
-#                   Use the parameter zeros_y_values for the y values of the zeros
 class GraphicProperties:
-    def __init__(self, title, x_title, y_title, x_value_array, y_value_array, graphic_type, zeros_x_values=None,
-                 zeros_y_values=None):
+    def __init__(self, title, x_title, y_title, x_value_array, y_value_array, graphic_type):
         self.title = title
         self.xValueArray = x_value_array
         self.yValueArray = y_value_array
         self.graphicType = graphic_type
         self.xTitle = x_title
         self.yTitle = y_title
-        self.zerosXValues = zeros_x_values
-        self.zerosYValues = zeros_y_values
 
 
 class GraphicTypes(Enum):
-    BODE = 0
-    LINEAL = 1
-    zero_POLES = 2
+    BodeModule = "BodeModule"
+    BodePhase = "BodePhase"
+    Output = "Output"
+    Zeros = "Zeros"
+    Poles = "Poles"
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-    a = [10, 20, 30, 40, 75, 95, 120]
-    b = [60, -70, 80, 90, 65, 88, 77]
-    c = [10, 50, 80, 99, 120, 180, 222, 4000, 84444, 95555, 3333333, 5555555555555]
-    d = [20, 45, -88, 100, -151, 174, 188, 555, 800, 1050, 9999, 400]
-    graphic1 = GraphicProperties("Salida 1", "Valores x", "Valores y", a, b, GraphicTypes.zero_POLES,
-                                 [88, 120, 500], [-50, 50, 0])
-    graphic2 = GraphicProperties("Salida 2", "Valores x", "Valores y", c, d, GraphicTypes.BODE)
 
-    graphics = [graphic1, graphic2]
-    window = OutputGraphics(graphics)
+    window = OutputGraphics()
     window.show()
     app.exec()
